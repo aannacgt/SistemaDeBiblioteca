7b9c304fd9d077505f6ae7168ee959df7bffa349
Victor Luiz <139981746+vluizlr@users.noreply.github.com>
2025-01-14 01:06:15 -0300

    Ultima Revisao

--- test_livro.py
+++ test_livro.py
@@ -1,3 +1,6 @@
+# Este teste verifica o comportamento da classe 'Livro' em relação à disponibilidade de um livro.
+# Ele simula um ciclo completo de empréstimo e devolução, onde o estado de disponibilidade do livro é monitorado em cada etapa.
+# O objetivo é garantir que a classe 'Livro' esteja manipulando corretamente a disponibilidade do livro durante o ciclo de empréstimo e devolução.
 from src.livro import Livro
 
 livro = Livro("1984", "George Orwell")
